9519ca1ed7c9bc2940ab851cdad29fb430817de8
Igor Grankin <i.grankin@dodopizza.com>
2022-08-12 20:42:48 +0300

    Initial Commit

---
+++ Hangman kata/Hangman_kata.h
@@ -0,0 +1,18 @@
+//
+//  Hangman_kata.h
+//  Hangman kata
+//
+//  Created by Игорь Гранкин on 12.08.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Hangman_kata.
+FOUNDATION_EXPORT double Hangman_kataVersionNumber;
+
+//! Project version string for Hangman_kata.
+FOUNDATION_EXPORT const unsigned char Hangman_kataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Hangman_kata/PublicHeader.h>
+
+
